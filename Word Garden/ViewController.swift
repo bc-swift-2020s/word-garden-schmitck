9f3c2b934c75d89d146118f2f4c6e4cf311ff1b1
schmitck <schmitck@bc.edu>
2020-02-01 12:39:50 -0500

    Finished CH 3.3 - Optionals

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -17,7 +17,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var flowerImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        guessLetterButton.isEnabled = false
+        playAgainButton.isHidden = true
     }
     
 func updateUIAfterGuess(){
@@ -26,6 +27,19 @@ func updateUIAfterGuess(){
     }
     
     @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        if let letterGuessed = guessedLetterField.text?.last {
+            //if both the text and the last character is not nil, then we assign the last character to guessedLetterField.text
+            guessedLetterField.text = String(letterGuessed)
+            // remember we have to assign the string element to the character - can also use string interpolation
+            guessLetterButton.isEnabled = true
+        } else {
+            //if one of the types above (.text or .last), then we go to this step
+            guessLetterButton.isEnabled = false
+            // this disables the button so it WILL NOT BE ABLE TO BE CLICKED
+            // this is what happens when there is no character in the letterfield
+
+            
+        }
     }
     
     
